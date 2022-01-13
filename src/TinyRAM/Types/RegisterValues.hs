541b3a128fac051f8321515d5258b566884f9860
Morgan Thomas <morgan.a.s.thomas@gmail.com>
2022-01-13 14:28:15 -0500

    add comment

--- src/TinyRAM/Types/RegisterValues.hs
+++ src/TinyRAM/Types/RegisterValues.hs
@@ -11,6 +11,8 @@ import TinyRAM.Types.Register (Register)
 import TinyRAM.Types.Word (Word)
 
 
+-- A state of the registers, giving the word
+-- stored in each one.
 newtype RegisterValues = RegisterValues
   { unRegisterValues :: Map Register Word }
   deriving (Eq, Ord, Read, Show, Generic)
