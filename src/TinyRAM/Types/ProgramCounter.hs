cea5e8cb60dea5c7791dce5f16d90360f85e9ee1
Morgan Thomas <morgan.a.s.thomas@gmail.com>
2022-01-13 14:22:39 -0500

    ProgramCounter type

---
+++ src/TinyRAM/Types/ProgramCounter.hs
@@ -0,0 +1,15 @@
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE NoImplicitPrelude #-}
+
+
+module TinyRAM.Types.ProgramCounter ( ProgramCounter (..) ) where
+
+
+import TinyRAM.Prelude
+import TinyRAM.Types.Address (Address)
+
+
+-- An address representing a value of the program counter.
+newtype ProgramCounter = ProgramCounter { unProgramCounter :: Address }
+  deriving (Eq, Ord, Read, Show, Generic, Enum, Num, Real, Integral)
