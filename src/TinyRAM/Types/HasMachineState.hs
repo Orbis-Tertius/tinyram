88006f7b7157c5d603ed5e08b95cdc73f3e876a2
Morgan Thomas <morgan.a.s.thomas@gmail.com>
2022-01-13 16:08:19 -0500

    HasMachineState type class

---
+++ src/TinyRAM/Types/HasMachineState.hs
@@ -0,0 +1,27 @@
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE NoImplicitPrelude #-}
+
+
+module TinyRAM.Types.HasMachineState ( HasMachineState (..) ) where
+
+
+import TinyRAM.Prelude
+import TinyRAM.Types.Address (Address)
+import TinyRAM.Types.Flag (Flag)
+import TinyRAM.Types.ProgramCounter (ProgramCounter)
+import TinyRAM.Types.Register (Register)
+import TinyRAM.Types.Word (Word)
+
+
+class HasMachineState m where
+  getProgramCounter :: m ProgramCounter
+  setProgramCounter :: ProgramCounter -> m ()
+  getRegisterValue :: Register -> m (Maybe Word)
+  setRegisterValue :: Register -> Word -> m ()
+  getConditionFlag :: m Flag
+  setConditionFlag :: Flag -> m ()
+  getMemoryValue :: Address -> m (Maybe Word)
+  setMemoryValue :: Address -> Word -> m ()
+  readPrimaryInput :: m (Maybe Word)
+  readAuxiliaryInput :: m (Maybe Word)
