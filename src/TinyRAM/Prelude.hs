437abcf6dea64e4c36261b9f6f2eba9e4568e388
Morgan Thomas <morgan.a.s.thomas@gmail.com>
2022-01-13 16:50:12 -0500

    instance HasMachineState TinyRAMT

--- src/TinyRAM/Prelude.hs
+++ src/TinyRAM/Prelude.hs
@@ -3,6 +3,7 @@
 
 module TinyRAM.Prelude
   ( module Prelude
+  , module Control.Lens
   , module Control.Monad.Trans.Class
   , module Data.Map
   , module GHC.Generics
@@ -10,6 +11,8 @@ module TinyRAM.Prelude
 
 
 import Prelude hiding (Word)
+import Control.Lens ((^.), (.~))
 import Control.Monad.Trans.Class (MonadTrans (lift))
+import Data.Generics.Labels ()
 import Data.Map (Map)
 import GHC.Generics (Generic)
--- src/TinyRAM/Types/TinyRAMT.hs
+++ src/TinyRAM/Types/TinyRAMT.hs
@@ -1,15 +1,22 @@
 {-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
 {-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE OverloadedLabels #-}
+{-# LANGUAGE TypeApplications #-}
 
 
 module TinyRAM.Types.TinyRAMT ( TinyRAMT (..) ) where
 
 
-import Control.Monad.Trans.State (StateT)
+import Control.Monad.Trans.State (StateT, gets, modify)
+import qualified Data.Map as Map
 
 import TinyRAM.Prelude
+import TinyRAM.Types.HasMachineState (HasMachineState (..))
+import TinyRAM.Types.InputTape (InputTape (InputTape), Primary, Auxiliary)
 import TinyRAM.Types.MachineState (MachineState)
+import TinyRAM.Types.MemoryValues (MemoryValues (..))
+import TinyRAM.Types.RegisterValues (RegisterValues (..))
 
 
 newtype TinyRAMT m a = TinyRAMT { unTinyRAMT :: StateT MachineState m a }
@@ -28,3 +35,37 @@ instance Monad m => Applicative (TinyRAMT m) where
 instance Monad m => Monad (TinyRAMT m) where
   return = TinyRAMT . return
   (TinyRAMT x) >>= f = TinyRAMT $ x >>= (unTinyRAMT . f)
+
+instance Monad m => HasMachineState (TinyRAMT m) where
+  getProgramCounter = TinyRAMT $ gets (^. #programCounter)
+  setProgramCounter pc = TinyRAMT $ modify (#programCounter .~ pc)
+  getRegisterValue r = TinyRAMT $ gets (Map.lookup r . (^. #registerValues . #unRegisterValues))
+  setRegisterValue r w =
+    TinyRAMT $ modify
+      (\s -> #registerValues
+        .~ RegisterValues (Map.insert r w (s ^. #registerValues . #unRegisterValues))
+        $ s
+      )
+  getConditionFlag = TinyRAMT $ gets (^. #conditionFlag)
+  setConditionFlag flag = TinyRAMT $ modify (#conditionFlag .~ flag)
+  getMemoryValue addr = TinyRAMT $ gets (Map.lookup addr . (^. #memoryValues . #unMemoryValues))
+  setMemoryValue addr w =
+    TinyRAMT $  modify
+      (\s -> #memoryValues
+         .~ MemoryValues (Map.insert addr w (s ^. #memoryValues . #unMemoryValues))
+         $ s
+       )
+  readPrimaryInput = TinyRAMT $ do
+    input <- gets (^. #primaryInput . #unInputTape)
+    case input of
+      [] -> return Nothing
+      (w:input') -> do
+        modify (#primaryInput .~ InputTape @Primary input')
+        return (Just w)
+  readAuxiliaryInput = TinyRAMT $ do
+    input <- gets (^. #auxiliaryInput . #unInputTape)
+    case input of
+      [] -> return Nothing
+      (w:input') -> do
+        modify (#auxiliaryInput .~ InputTape @Auxiliary input')
+        return (Just w)
