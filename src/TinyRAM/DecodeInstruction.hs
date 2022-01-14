94a8e99cf7d33945099eec68bddca77e4739ff7c
Morgan Thomas <morgan.a.s.thomas@gmail.com>
2022-01-14 17:36:58 -0500

    stub instruction decoder

---
+++ src/TinyRAM/DecodeInstruction.hs
@@ -0,0 +1,16 @@
+{-# LANGUAGE NoImplicitPrelude #-}
+
+
+module TinyRAM.DecodeInstruction ( decodeInstruction ) where
+
+
+import TinyRAM.Types.Instruction (Instruction)
+import TinyRAM.Types.Word (Word)
+
+
+decodeInstruction :: (Word, Word) -> Instruction
+decodeInstruction = todo
+
+
+todo :: a
+todo = todo
