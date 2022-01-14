4cc14f6abd7073ed6a2a083b228977fbc72e8337
Morgan Thomas <morgan.a.s.thomas@gmail.com>
2022-01-14 18:14:14 -0500

    add max steps to emulator

--- src/TinyRAM/Run.hs
+++ src/TinyRAM/Run.hs
@@ -11,12 +11,14 @@ import TinyRAM.MachineState (getImmediateOrRegister)
 import TinyRAM.Prelude
 import TinyRAM.Types.HasMachineState (HasMachineState (..))
 import TinyRAM.Types.HasParams (HasParams)
+import TinyRAM.Types.MaxSteps (MaxSteps (..))
 import TinyRAM.Types.ProgramCounter (ProgramCounter (..))
 import TinyRAM.Types.Word (Word)
 
 
-run :: ( Monad m, HasMachineState m, HasParams m ) => m Word
-run = do
+run :: ( Monad m, HasMachineState m, HasParams m ) => Maybe MaxSteps -> m Word
+run (Just 0) = return ()
+run n = do
   pc <- unProgramCounter <$> getProgramCounter
   i0 <- getMemoryValue pc
   i1 <- getMemoryValue (pc+1)
@@ -28,8 +30,8 @@ run = do
           a <- getImmediateOrRegister (i ^. #a)
           case a of
             Just a' -> return a'
-            _ -> run
+            _ -> run ((-1) <$> n)
         else do
           executeInstruction i
-          run
-    _ -> run
+          run ((-1) <$> n)
+    _ -> run ((-1) <$> n)
---
+++ src/TinyRAM/Types/MaxSteps.hs
@@ -0,0 +1,12 @@
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE NoImplicitPrelude #-}
+
+
+module TinyRAM.Types.MaxSteps ( MaxSteps (..) ) where
+
+
+-- The maximum number of instructions which the emulator is allowed
+-- to execute in running a program.
+newtype MaxSteps = MaxSteps { unMaxSteps :: Integer }
+  deriving (Eq, Ord, Read, Show, Generic, Enum, Num, Real, Integral)
