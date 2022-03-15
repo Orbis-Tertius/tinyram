6554751f050f2377ac972eb436d58a24c619f404
Derek Verbrugge <derek.verbrugge@platonic.systems>
2022-03-15 13:02:30 -0400

    automate the simple test case

--- test/TinyRAM/Spec/AssemblerSpec.hs
+++ test/TinyRAM/Spec/AssemblerSpec.hs
@@ -23,15 +23,16 @@ import           TinyRAM.Types.Word            (Word)
 
 spec :: Spec
 spec = describe "assembler/run" $
-  before (handleCommand (CommandParse (ProgramFilePath "examples/max.s") (ProgramFilePath "examples/maxoutput.txt"))) $
+  before (handleCommand (CommandParse (ProgramFilePath "examples/max.s") objectFilePath)) $
     it "assembler/run max program and test against inputs" $ do
       forAll (genWord 16) $ \(a :: Word) ->
         forAll (listOf $ resize (fromIntegral a) $ genWord 15) $ \(wrds :: [Word]) -> do
             let params = Params 16 16 -- hardcoded assuming won't be changed in examples/max.s
-                programFilePath = ProgramFilePath "examples/maxoutput.txt"
-            program <- Program . BC.concat . BC.lines . unProgram <$> readProgramFile programFilePath
+            program <- Program . BC.concat . BC.lines . unProgram <$> readProgramFile objectFilePath
             let answer = executeProgram params (Just 1000) program (InputTape wrds) (InputTape [])
             answer `shouldBe` (Right $ maximum' $ wrds)
+  where
+    objectFilePath = ProgramFilePath "examples/max.o"
 
 maximum' :: [Word] -> Word
 maximum' [] = 0
