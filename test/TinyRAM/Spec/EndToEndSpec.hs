e41774090521bee6c677bb46076ec8cf683b9c9f
Derek Verbrugge <derek.verbrugge@platonic.systems>
2022-10-18 16:33:17 -0400

    linted

--- test/TinyRAM/Spec/EndToEndSpec.hs
+++ test/TinyRAM/Spec/EndToEndSpec.hs
@@ -79,7 +79,8 @@ spec = describe "TinyRAM end to end" $ do
   --cmpgeTestCaseL --bugged reported
   cmpgeNegTestCaseG
   cmpgeNegTestCaseE
-  --cmpgeNegTestCaseL
+
+--cmpgeNegTestCaseL
 
 --answerR1TestCase --bugged reported
 
@@ -276,7 +277,7 @@ cmpaeTestCaseG =
 --cmpae r2, r3
 --cmov r1, 1
 --answer r1
-  
+
 --; TinyRAM V=1.000 W=16 K=16
 --mov r1, 0
 --mov r2, 1
