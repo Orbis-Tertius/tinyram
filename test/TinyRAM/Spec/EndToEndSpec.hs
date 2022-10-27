b16a13e35cb4505c7d44647ecbb3d07ec6c249cd
Derek Verbrugge <derek.verbrugge@platonic.systems>
2022-10-27 15:48:18 -0400

    demonstrates bug with xorPreFlagTestCase

--- test/TinyRAM/Spec/EndToEndSpec.hs
+++ test/TinyRAM/Spec/EndToEndSpec.hs
@@ -45,6 +45,7 @@ spec = describe "TinyRAM end to end" $ do
   orTestCase
   orFlagTestCase
   --xorTestCase --bugged reported
+  xorPreFlagTestCase
   addTestNegativeTestCase
   subTestCase
   --notTestCase --negative answer bugged
@@ -755,6 +756,19 @@ orFlagTestCase =
 --             ]
 --     answer <- execute program (InputTape []) (InputTape [])
 --     answer `shouldBe` Right 15
+
+xorPreFlagTestCase :: Spec
+xorPreFlagTestCase =
+  it "answers 15" $ do
+    let program =
+          construct
+            [ Mov (reg' 2) (imm 15),
+              Cmov (reg' 0) (imm 1),
+              Xor (reg' 0) (reg' 2) (reg 2),
+              Answer (reg 0)
+            ]
+    answer <- execute program (InputTape []) (InputTape [])
+    answer `shouldBe` Right 15
 
 addTestNegativeTestCase :: Spec
 addTestNegativeTestCase =
